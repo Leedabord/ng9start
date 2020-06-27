d093655ced325d8792277bf9d52bc1e762a048fc
Leedabord <gwforumlakers@gmail.com>
2020-06-27 11:53:38 -0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {}
+export class AppComponent {
+  title = 'angular-router-sample';
+}
 
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
+/* */
