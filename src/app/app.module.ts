67ffda9900be7aaa7b4ed9b99ac58dfc5ce02642
Leedabord <gwforumlakers@gmail.com>
2020-06-27 12:57:00 -0700

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from "@angular/forms";
 
-//import { AppRoutingModule } from "./app-routing.module";
+import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
