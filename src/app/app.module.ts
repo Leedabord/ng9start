c66cf7f680b809939be95d369767290394c5ac6e
Leedabord <gwforumlakers@gmail.com>
2020-06-27 12:03:53 -0700

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,6 @@ import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-
 @NgModule({
   imports: [
     BrowserModule,
